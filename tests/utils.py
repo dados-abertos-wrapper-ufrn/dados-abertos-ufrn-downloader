1eb95c004fee278ee10398c755c68b636beed484
Breno Baiardi <brenocbo@gmail.com>
2019-10-06 21:34:06 -0300

    corrigindo styling

--- tests/utils.py
+++ tests/utils.py
@@ -3,9 +3,10 @@ import os
 import sys
 import shutil
 import unittest
+from odufrn_downloader import ODUFRNDownloader
 from os.path import dirname, join, abspath
 sys.path.insert(0, abspath(join(dirname(__file__), '..')))
-from odufrn_downloader import ODUFRNDownloader
+
 
 def input_value(fun):
     """Recebe função que imprime algo na tela e retorna impressao."""
@@ -15,7 +16,7 @@ def input_value(fun):
     sys.stdout = sys.__stdout__
     return capturedOutput.getvalue()
 
-def assert_console(fun, message:str):
+def assert_console(fun, message: str):
     """Recebe função que printa algo na tela e realiza assert
     que verifica se foi printado."""
     unit = unittest.TestCase()
